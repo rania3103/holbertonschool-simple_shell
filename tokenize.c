e3fdd24a10bc498b87c48b4db07f94f3817f51f9
rania3103 <raniaabassi33@gmail.com>
2023-08-23 20:15:04 +0100

    add exit and remove checking errors

--- tokenize.c
+++ tokenize.c
@@ -6,34 +6,22 @@
 */
 char **tokenize(char *line)
 {
-	char **tokens;
+	char **tokens = NULL;
 	char *token;
-	int i, j;
 
-	i = 0;
 	tokens = (char **)malloc(sizeof(char *) * 1024);
 	if (tokens == NULL)
 	{
-		perror("error in memery allocation");
-		exit(1);
+		perror("error in memory allocation");
+		exit(EXIT_FAILURE);
 	}
 	token = strtok(line, " \n\t\r\a");
 	while (token != NULL)
 	{
-		tokens[i] = strdup(token);
-		if (tokens[i] == NULL)
-		{
-			perror("error in memory allocation");
-			exit(1);
-		}
+		tokens[i] = token;
 		i++;
 		token = strtok(NULL, " \n\t\r\a");
 	}
 	tokens[i] = NULL;
-	for (j = 0; j < i; j++)
-	{
-    	free(tokens[j]);
-	}
-	free(tokens);
 	return (tokens);
 }
