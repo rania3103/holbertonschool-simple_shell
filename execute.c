48f0036c8f8e46221131bf7ae67a4a0f56210fdb
rania3103 <raniaabassi33@gmail.com>
2023-08-24 12:37:22 +0100

    fix betty

--- execute.c
+++ execute.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * execute - execute a command
- * @args: arguments
+ * @line_input: line
  * Return: 1
 */
 int execute(char *line_input)
@@ -9,20 +9,15 @@ int execute(char *line_input)
 	char **args;
 	char *path;
 	pid_t pid;
-	int exit_stat = 0; 
-	int status;
+	int status, exit_stat = 0;
 
 	args = tokenize(line_input);
 	if (args == NULL)
 		return (-1);
 	if (line_input[0] == '/')
-	{
 		path = strdup(line_input);
-	}
 	else
-	{
 		path = get_path(args[0]);
-	}
 	if (path == NULL)
 	{
 		free(args);
@@ -36,18 +31,14 @@ int execute(char *line_input)
 		return (-1);
 	}
 	else if (pid == 0)
-	{
 		exit_stat = execve(path, args, environ);
-	}
 	else
 	{
 		wait(&status);
 		free(args);
 		free(path);
 		if (WIFEXITED(status))
-		{
 			status = WEXITSTATUS(status);
-		}
 	}
 	return (exit_stat);
 }
