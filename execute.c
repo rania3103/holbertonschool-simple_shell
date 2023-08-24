d5bdc8feb4abd6151f8847fd09ebebaa09c4096a
rania3103 <raniaabassi33@gmail.com>
2023-08-24 12:29:32 +0100

    fix path

--- execute.c
+++ execute.c
@@ -4,61 +4,50 @@
  * @args: arguments
  * Return: 1
 */
-int execute(char **args)
+int execute(char *line_input)
 {
-	char **path;
-	int i, status;
-	struct stat st;
+	char **args;
+	char *path;
+	pid_t pid;
+	int exit_stat = 0; 
+	int status;
 
-	if (strcmp(args[0], "env") == 0)
+	args = tokenize(line_input);
+	if (args == NULL)
+		return (-1);
+	if (line_input[0] == '/')
 	{
-		print_env();
-		return (0);
+		path = strdup(line_input);
 	}
-
-	path = tokenize_path(getenv("PATH"));
+	else
+	{
+		path = get_path(args[0]);
+	}
 	if (path == NULL)
 	{
-		perror("failed to retrieve path");
-		free(path);
-		return (1);
+		free(args);
+		return (-1);
 	}
-	for (i = 0; path[i] != NULL; i++)
+	pid = fork();
+	if (pid < 0)
 	{
-		char *buffer = construct_buffer(path[i], args[0]);
-
-		if (stat(buffer, &st) == 0)
+		free(args);
+		free(path);
+		return (-1);
+	}
+	else if (pid == 0)
+	{
+		exit_stat = execve(path, args, environ);
+	}
+	else
+	{
+		wait(&status);
+		free(args);
+		free(path);
+		if (WIFEXITED(status))
 		{
-			pid_t pid = fork();
-
-			if (pid == -1)
-			{
-				perror("Error in fork");
-				free(buffer);
-				free(path);
-				return (1);
-			}
-			else if (pid == 0)
-			{
-				if (execve(buffer, args, environ) == -1)
-				{
-					perror("Error in execve");
-					free(buffer),
-					free(path);
-					return (1);
-				}
-				exit(EXIT_FAILURE);
-			}
-			else
-			{
-				do {
-					waitpid(pid, &status, WUNTRACED);
-				} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-				free(buffer);
-			}
+			status = WEXITSTATUS(status);
 		}
 	}
-	perror("command not found");
-	free(path);
-	return (1);
+	return (exit_stat);
 }
