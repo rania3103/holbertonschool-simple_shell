5af72f80a12669e1b195361ee10c3b9fcdb97daa
MariamCHARRADA <charradamariam@yahoo.com>
2023-08-25 02:40:25 -0700

    add comments

--- execute.c
+++ execute.c
@@ -14,33 +14,33 @@ int execute(char *line_input)
 
 	args = tokenize(line_input);
 	if (args == NULL)
-		return (-1);
+		return (-1); /** Return -1 if tokenization fails */
 
 	if (line_input[0] == '/')
-		path = strdup(line_input);
+		path = strdup(line_input); /** Use the input as path if it starts with '/' */
 	else
 		path = get_path(args[0]);
-	if (path == NULL)
+	if (path == NULL) /** Failed to retreive path */
 	{
 		free_tokens(args);
 		return (-1);
 	}
-	pid = fork();
-	if (pid < 0)
+	pid = fork(); /** Create a child process */
+	if (pid < 0) /** Fork failure */
 	{
 		free_tokens(args);
 		free(path);
 		return (-1);
 	}
-	else if (pid == 0)
+	else if (pid == 0)  /** Child process */
 		exit_stat = execve(path, args, environ);
-	else
+	else  /** Parent process */
 	{
 		wait(&status);
 		free(path);
 		free(args);
 		if (WIFEXITED(status))
-			status = WEXITSTATUS(status);
+			status = WEXITSTATUS(status); /** Get child process exit status */
 	}
 	free_tokens(args);
 	free(path);
